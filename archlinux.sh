ce95097849d982b0c5bd4bb0f445cbf157db8db6
Azathothas <AjamX101@gmail.com>
2025-01-18 12:27:50 +0545

    more

--- archlinux.sh
+++ archlinux.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #
 ##DO NOT RUN DIRECTLY
-##Self: bash <(curl -qfsSL "https://raw.githubusercontent.com/pkgforge-dev/runimage-base/refs/heads/main/alpine.sh")
-# HOST: aarch64-Linux, x86_64-Linux
+##Self: bash <(curl -qfsSL "https://raw.githubusercontent.com/pkgforge-dev/runimage-base/refs/heads/main/archlinux.sh")
+# HOST: x86_64-Linux
 #-------------------------------------------------------#
 
 #-------------------------------------------------------#
@@ -14,7 +14,7 @@
  chmod -v 'a+x' "./runimage"
  [[ -s "./runimage" ]] || exit 1
 ##Get Rootfs
- "./runimage" getdimg --extract rootfs "pkgforge/alpine-base:$(uname -m)"
+ "./runimage" getdimg --extract rootfs "pkgforge/archlinux-base:$(uname -m)"
  RIM_NO_NET="0"
  RIM_NO_NVIDIA_CHECK="1"
  RIM_OVERFS_MODE="1"
@@ -32,23 +32,19 @@
     echo -e "nameserver 8.8.8.8\nnameserver 2620:0:ccc::2" | tee "/etc/resolv.conf"
     echo -e "nameserver 1.1.1.1\nnameserver 2606:4700:4700::1111" | tee -a "/etc/resolv.conf"
    #Requirements
-    BASE_PKGS=(bash binutils coreutils curl file findutils gawk gocryptfs grep gzip iproute2 iptables iputils jq kmod libnotify lsof lz4 nftables openresolv patchelf procps-ng slirp4netns sed socat tar util-linux which xhost xz zstd)
-    apk update --no-interactive
-    apk upgrade --no-interactive
-    #apk add "${BASE_PKGS[@]}" --no-cache --latest --upgrade --no-interactive
-    for pkg in "${BASE_PKGS[@]}"; do apk add "${pkg}" --no-cache --latest --upgrade --no-interactive 2>/dev/null; done
+    BASE_PKGS=(bash binutils coreutils curl file findutils gawk gocryptfs grep gzip iproute2 iptables iputils jq kmod libnotify lsof lz4 nftables openresolv patchelf procps-ng slirp4netns sed socat tar util-linux which xorg-xhost xz zstd)
+    pacman -Scc --noconfirm ; rm "/var/lib/pacman/sync/"*
+    pacman -y --sync --refresh --refresh --sysupgrade --noconfirm
+    #pacman -S "${BASE_PKGS[@]}" --needed --noconfirm 
+    for pkg in "${BASE_PKGS[@]}"; do pacman -S "${pkg}" --needed --noconfirm 2>/dev/null; done
    #Cleanup
-    chmod 755 "/bin/bbsuid"
-    rm -rfv "/var/cache/apk/"* 2>/dev/null
-    rm -rfv "/usr/share/fonts/"* 2>/dev/null
-    rm -rfv "/usr/share/licenses/"* 2>/dev/null
-    rm -rfv "/usr/share/locale/"* 2>/dev/null
-    rm -rfv "/usr/share/man/"* 2>/dev/null
-    apk cache clean
-    ln -s "/dev/null" "/etc/apk/cache"
+    pacman -Rsn base-devel --noconfirm 2>/dev/null
+    pacman -Rsn perl --noconfirm 2>/dev/null
+    pacman -Rsn python --noconfirm 2>/dev/null
+    pacman -Scc --noconfirm 2>/dev/null
     rim-shrink --all --verbose 2>/dev/null
    #Rebuild [Dwarfs ZSTD 22]
-    rim-build --bsize '22' --clvl '22' --dwfs "/tmp/runimage"
+    rim-build --bsize '24' --clvl '22' --dwfs "/tmp/runimage"
   }
  export -f build_image
 ##Rebuild
