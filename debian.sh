62ec6eefadb592afe6ccfd189380d11eabf932af
Azathothas <AjamX101@gmail.com>
2025-01-18 16:18:31 +0545

    fix

--- debian.sh
+++ debian.sh
@@ -31,13 +31,15 @@
     echo -e "nameserver 8.8.8.8\nnameserver 2620:0:ccc::2" | tee "/etc/resolv.conf"
     echo -e "nameserver 1.1.1.1\nnameserver 2606:4700:4700::1111" | tee -a "/etc/resolv.conf"
    #Requirements
+    DEBIAN_FRONTEND="noninteractive"
     BASE_PKGS=(bash binutils coreutils curl file findutils gawk gocryptfs grep gzip iproute2 iptables iputils-ping jq kmod libnotify-bin lsof liblz4-1 nftables openresolv patchelf procps socat tar util-linux which x11-xserver-utils xz-utils zstd)
     DEBIAN_FRONTEND="noninteractive" apt clean -y
     DEBIAN_FRONTEND="noninteractive" apt update -y
     #DEBIAN_FRONTEND="noninteractive" apt install -f "${BASE_PKGS[@]}" -y --no-install-recommends --ignore-missing
     for pkg in "${BASE_PKGS[@]}"; do DEBIAN_FRONTEND="noninteractive" apt install -f "${pkg}" -y --no-install-recommends --ignore-missing 2>/dev/null; done
    #Cleanup
-    curl -qfsSL "https://raw.githubusercontent.com/VHSgunzo/runimage-fake-sudo-pkexec/refs/heads/main/usr/bin/sudo" -o "/usr/bin/sudo" && chmod -v "a+x" "/usr/bin/sudo"
+    curl -kqfsSL "https://raw.githubusercontent.com/VHSgunzo/runimage-fake-sudo-pkexec/refs/heads/main/usr/bin/sudo" -o "./sudo"
+    mv -fv "./sudo" "/usr/bin/sudo" && chmod -v "a+x" "/usr/bin/sudo"
     apt purge locales perl -y ; apt autoremove -y ; apt autoclean -y
     apt list --installed
     apt clean -y
